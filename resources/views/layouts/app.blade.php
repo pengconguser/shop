23930e18ff58651c3f16f28367b09247953b5ea1
peng <531299585@qq.com>
2018-08-02 13:51:03 +0800

    fisrt commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF TOKEN -->
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>@yield('title','shop')-smart shop</title>
-	{{-- 样式 --}}
-	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
+    <!-- 样式 -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
